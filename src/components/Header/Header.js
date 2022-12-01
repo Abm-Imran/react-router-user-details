0d60350720f6c91740a7ab6324af0d8d02dc9c43
unknown <Abm-Imran>
2022-12-01 11:29:16 +0600

    Active route link color added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
             <h1>Welcome to React Routing...</h1>
-            <nav>
-                <Link to='/'>Home</Link>
-                <Link to='/friends'>Friends</Link>
-                <Link to='/about'>About</Link>
+            <nav className='nav-bar'>
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/friends'>Friends</NavLink>
+                <NavLink to='/about'>About</NavLink>
                 
             </nav>
         </div>
